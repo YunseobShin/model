b87b1ec3a66a779b2c13838fe5c71f994c13752c
yunseobshin <skwi2004@gmail.com>
2016-04-14 18:23:20 +0000

    post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,15 @@ class HomeController < ApplicationController
     
   end
   def write
-    redirect_to "/list"
-    @title = params[:title]
-    @content = params[:content]
+    new_post = Post.new
+    #,title은 migrate의 title과 같아야함
+    new_post.title = params[:title]
+    new_post.content = params[:content]
+    new_post.save
     
+    redirect_to "/list"
   end
   def list
-    
+    @every_post = Post.all.order("id desc")
   end
 end
